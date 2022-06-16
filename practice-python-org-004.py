776969779d77ad5c3e19fbddc7a7ab4452779e14
Gosia <gosia@gmail.com>
2022-06-16 22:30:15 +0200

    Zmiana nazwy

--- practice-python-org-004.py
+++ practice-python-org-004.py
@@ -1,10 +1,8 @@
 print("Podaj liczbę: ")
-a = int(input())
+user_input = int(input())
 
 #x = range(1,a,1)
-for n in range (1,a+1):
+for element in range (1,user_input+1):
 #    print(n)
-    if (a % n) == 0:
-        print(n)
-   
-    
+    if (user_input % element) == 0:
+        print(element)
