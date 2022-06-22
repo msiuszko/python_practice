8e0c468553a8ad8d72b5f8aa794ce8bc9c127017
Gosia <gosia@gmail.com>
2022-06-22 22:07:16 +0200

    Rozwiązanie zadania 4

--- practice-python-org-004.py
+++ practice-python-org-004.py
@@ -1,8 +1,7 @@
-print("Podaj liczbę: ")
+#wyświetlanie liczby podanej przez użytkownika i wyświetlanie wszystkich dzielników
+print("Give the number: ")
 user_input = int(input())
 
-#x = range(1,a,1)
 for element in range (1,user_input+1):
-#    print(n)
     if (user_input % element) == 0:
         print(element)
